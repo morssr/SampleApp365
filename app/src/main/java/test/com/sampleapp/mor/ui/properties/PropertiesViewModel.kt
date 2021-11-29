7c85a4fd7f6c1e1ed8f646321d1b60b91d62caf8
Mor Shaar <morsrs1@gmail.com>
2021-11-29 20:51:06 +0200

    refactor

--- app/src/main/java/test/com/sampleapp/mor/ui/properties/PropertiesViewModel.kt
+++ app/src/main/java/test/com/sampleapp/mor/ui/properties/PropertiesViewModel.kt
@@ -39,7 +39,7 @@ constructor(
         ) {
             return lastResultFlow
         }
-        val skipInitialize = true
+        val skipInitialize = false
 
         //create new data stream
         val newResult =
